1f342768af700b640728b6d431870d9e21789e34
William Hearn <sylus1984@gmail.com>
2021-09-29 12:49:22 -0400

    feat(gatekeeper):  Add CPU and Memory Limits

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v1.0.9"
+  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v1.0.10"
 
   cluster_name = var.prefix
 
