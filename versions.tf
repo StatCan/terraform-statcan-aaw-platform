670d4de3b0a3a00ef8745a31b3619cd39c960796
Justin Bertrand <justin.bertrand@canada.ca>
2022-06-21 16:05:13 +0000

    fix: add azurerm required provider and set min and max.

--- versions.tf
+++ versions.tf
@@ -1,14 +1,19 @@
 
 terraform {
   required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0.0, < 3.0.0"
+    }
+
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
+      version = ">= 2.0.0, < 3.0.0"
     }
 
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0.0"
+      version = ">= 2.0.0, < 3.0.0"
     }
   }
 }
