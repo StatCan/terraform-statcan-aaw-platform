f2239461d4e8f75ac8d86ab0c462ec5739a08e58
William Hearn <william.hearn@canada.ca>
2022-08-01 13:37:15 -0400

    feat(ingress) VirtualService exists so rm Ingress

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://github.com/statcan/terraform-statcan-kubernetes-core-platform.git?ref=v1.5.4"
+  source = "git::https://github.com/statcan/terraform-statcan-kubernetes-core-platform.git?ref=v1.5.5"
 
   cluster_name = var.prefix
 
