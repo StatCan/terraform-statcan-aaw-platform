cbe8c7998b669187c97b8d07c85a43f930adc786
Adam Glavine <adam.glavine@cloud.statcan.ca>
2022-08-25 16:43:42 +0000

    feat-velero update to new version of core_platform v1.6.5

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://github.com/statcan/terraform-statcan-kubernetes-core-platform.git?ref=v1.6.4"
+  source = "git::https://github.com/statcan/terraform-statcan-kubernetes-core-platform.git?ref=v1.6.5"
 
   cluster_name = var.prefix
 
