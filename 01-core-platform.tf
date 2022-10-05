962d2b604596672b530ea8d1f2c5ea7be4924bc9
Julie Dion <julie.dion@statcan.gc.ca>
2022-10-05 19:37:16 +0000

    Update cert-manager to 1.9.1

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://github.com/statcan/terraform-statcan-kubernetes-core-platform.git?ref=v2.0.0"
+  source = "git::https://github.com/statcan/terraform-statcan-kubernetes-core-platform.git?ref=v2.0.1"
 
   cluster_name = var.prefix
 
