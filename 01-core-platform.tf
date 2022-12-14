621b1485d5864503ae19a6b706d328fb226cb2e1
Julie Dion <julie.dion@statcan.gc.ca>
2022-12-14 09:53:27 -0500

    Upgrade fluentd-operator to 1.16.5, core platform v2.5.2

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v2.5.1"
+  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v2.5.2"
 
   cluster_name = var.prefix
 
