d832061b0a267db4b0c7389db30fa328d410fd10
William Hearn <william.hearn@canada.ca>
2023-01-20 09:10:56 -0500

    feat(CN-1605): Update KubeCost official Helm Chart to v1.99

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v2.8.0"
+  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v2.8.1"
 
   cluster_name = var.prefix
 
