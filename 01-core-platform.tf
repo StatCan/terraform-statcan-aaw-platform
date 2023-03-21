788ff91cf581204864247c71a4ec268850dc8e1e
Lukas Zabiela <lukas.zabiela@canada.ca>
2023-03-21 14:26:10 +0000

    feat(kube-prometheus-stack): fix containerwaiting, add containerimagepullproblem, add coredns alerts, add thanos destinationrule

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v2.13.1"
+  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v2.14.0"
 
   cluster_name = var.prefix
 
