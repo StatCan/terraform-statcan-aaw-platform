a8b1bb40dc6272cf88ceb01796ed3d7b2606e48e
Frances Zsurka <frances.zsurka@cloud.statcan.ca>
2023-06-28 18:19:08 +0000

    fix(statcan/core): correct labels for persistentvolume status alerts

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v2.21.0"
+  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v2.21.1"
 
   cluster_name = var.prefix
 
