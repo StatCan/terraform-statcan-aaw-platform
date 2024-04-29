bd6b9c4d05d40c44283587c68d1636799eb97a58
Kevin Do <kevin.do@cloud.statcan.ca>
2024-04-29 18:43:48 +0000

    feat(node_rules): report agentpool, standardize node label

--- 01-core-platform.tf
+++ 01-core-platform.tf
@@ -12,7 +12,7 @@ module "core_platform" {
     module.platform_infrastructure
   ]
 
-  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v3.1.4"
+  source = "git::https://gitlab.k8s.cloud.statcan.ca/cloudnative/terraform/modules/terraform-statcan-kubernetes-core-platform.git?ref=v3.1.5"
 
   cluster_name = var.prefix
 
